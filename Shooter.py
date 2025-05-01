ca64f1915575ff86bd15c20e3da15882f14f2833
MustaphaNSI <adammustapha.benazzouz@malraux-rabat.org>
2025-05-01 18:28:13 +0100

    Update Shooter.py

--- Shooter.py
+++ Shooter.py
@@ -1,3 +1,4 @@
+
 import pygame
 
 pygame.init()
@@ -8,15 +9,36 @@ imagelunette = pygame.transform.scale(imagelunette, (20,20))
 pygame.mouse.set_visible(False)
 check = False
 positionlunette = (-10,-10)
+bord1 = pygame.Rect(47,622,130,120)
+bord2 = pygame.Rect(210,622,380,120)
+bord3 = pygame.Rect(623,622,130,120)
+balle0 = pygame.image.load("Ammo0.png")
+balle1 = pygame.image.load("Ammo1.png")
+balle2 = pygame.image.load("Ammo2.png")
+balle3 = pygame.image.load("Ammo3.png")
+balle4 = pygame.image.load("Ammo4.png")
+balle5 = pygame.image.load("Ammo5.png")
+balle = [balle0,balle1,balle2,balle3,balle4,balle5]
+balles = 5
+sol = pygame.image.load("Sol.png")
+sol = pygame.transform.scale(sol, (800,301))
 
 def dessins():
-    global imagelunette, positionlunette
-    fenetre.fill((200,200,200))
+    global imagelunette, positionlunette, balles, bord1
+    fenetre.fill((125,125,200))
     fenetre.blit(imagelunette, positionlunette)
+    fenetre.blit(sol,(0,500))
+    pygame.draw.rect(fenetre, (0,0,150) , bord1, 8)
+    pygame.draw.rect(fenetre, (0,0,150) , bord2, 8)
+    pygame.draw.rect(fenetre, (0,0,150) , bord3, 8)
+    pygame.draw.rect(fenetre, (0,0,0) , (55,630,114,104))
+    pygame.draw.rect(fenetre, (0,0,0) , (218,630,364,104))
+    pygame.draw.rect(fenetre, (0,0,0) , (631,630,114,104))
+    fenetre.blit(balle[balles],(57,630))
     pygame.display.flip()
 
 def controles():
-    global positionlunette, check
+    global positionlunette, check, balles
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
            continuer = False
@@ -26,6 +48,7 @@ def controles():
        check = True
        pygame.mixer.music.load("tir.mp3")
        pygame.mixer.music.play()
+       balles = balles - 1
 
 clock = pygame.time.Clock()
 Continuer = True
@@ -35,4 +58,4 @@ while Continuer == True:
    controles()
    positionlunette = pygame.mouse.get_pos()
 
-pygame.quit()
+pygame.quit()    
