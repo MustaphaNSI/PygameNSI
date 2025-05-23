a86f218890629451acbd8e84cc330038967c673e
MustaphaNSI <adammustapha.benazzouz@malraux-rabat.org>
2025-05-23 20:06:37 +0100

    Update Shooter.py

--- Shooter.py
+++ Shooter.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import sys
 
 pygame.init()
 pygame.display.set_caption("Shoot")
@@ -10,6 +11,7 @@ check = False
 check1 = False
 check3 = False
 check4 = False
+check6 = False
 positionlunette = (-10,-10)
 bord0 = pygame.Rect(0,0,800,800)
 bord1 = pygame.Rect(47,622,130,120)
@@ -17,7 +19,10 @@ bord3 = pygame.Rect(623,622,130,120)
 bord4 = pygame.Rect(610,-10,250,50)
 bord5 = pygame.Rect(-10,-10,150,50)
 rectstart = pygame.Rect(250,500,287,87)
-rectquitter = pygame.Rect(300, 380, 200, 60)
+rectquitter = pygame.Rect(210, 500, 375, 85)
+facilerect = pygame.Rect(320, 300, 170, 55)
+moyenrect = pygame.Rect(320, 380, 170, 55)
+difficilerect = pygame.Rect(300, 460, 215, 55)
 balle0 = pygame.image.load("Ammo0.png")
 balle1 = pygame.image.load("Ammo1.png")
 balle2 = pygame.image.load("Ammo2.png")
@@ -33,6 +38,7 @@ logo = pygame.image.load("logo.png")
 logo = pygame.transform.scale(logo, (760,300))
 daydream20 = pygame.font.Font("Daydream.ttf",20)
 daydream27 = pygame.font.Font("Daydream.ttf",27)
+daydream40 = pygame.font.Font("Daydream.ttf",40)
 daydream50 = pygame.font.Font("Daydream.ttf",50)
 daydream65 = pygame.font.Font("Daydream.ttf",65)
 ducka1 = pygame.image.load("duckA1.png")
@@ -73,15 +79,14 @@ directions = []
 verticaldirection = []
 ducktypes = []
 ducketat = []
-duckrounds = [1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 ]
-vitesse = [10, 13, 10, 13, 16,10, 13, 16, 19, 22,10, 13, 16, 19, 22, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37,10, 13, 16, 19, 22, 25, 28, 31, 34, 37]
+duckrounds = [3, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6 ]
+vitesse = [10, 13, 10, 13, 16,10, 13, 16, 19, 22, 25]
 rounds = 0
-totducks = 148
+totducks = 60
 ducksmort = 0
 duckspawn = 0
 ducksuivit = 0
 d = vitesse[rounds]
-tempsdebut = pygame.time.get_ticks()
 
 def dessins():
     global imagelunette, positionlunette, balles, directions, ducks,  tempsecoule, ducksmort, duckspawn
@@ -138,7 +143,7 @@ def dessins():
     pygame.display.flip()
 
 def debut():
-    global rectstart, logo
+    global rectstart, logo,check3
     start = True
     pygame.mouse.set_visible(True)
     while start == True:
@@ -148,6 +153,10 @@ def debut():
         pygame.draw.rect(fenetre, (30,30,150), rectstart, 8)
         surfacestart = daydream50.render("START", True, (255, 255, 255))
         fenetre.blit(surfacestart, (rectstart[0] + 10, rectstart[1] + 10))
+        surfacetouche1 = daydream27.render("Click gauche : tirer", True, (255, 230, 255))
+        surfacetouche2 = daydream27.render("R : recharger", True, (255, 230, 255))
+        fenetre.blit(surfacetouche1, (18,710))
+        fenetre.blit(surfacetouche2, (18,750))
         pygame.display.flip()
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
@@ -158,9 +167,45 @@ def debut():
            check3 = True
            if rectstart.collidepoint(event.pos):
               start = False               
-    
+
+def leveled():
+    global facilerect, moyenrect, difficilerect,check6
+    choisir = True
+    while choisir == True:
+        fenetre.fill((20, 20, 20))
+        pygame.draw.rect(fenetre, (30,30,150) , bord0, 8)
+        choix = daydream40.render("Choisis ton niveau", True, (255, 255, 255))
+        fenetre.blit(choix, (80, 80))
+        pygame.draw.rect(fenetre, (0, 150, 0), facilerect,8)
+        textefacile = daydream27.render("Facile", True, (30, 30, 150))
+        fenetre.blit(textefacile, (facilerect[0] + 10, facilerect[1] + 10))
+        pygame.draw.rect(fenetre, (200, 150, 0), moyenrect,8)
+        textemoyen = daydream27.render("Moyen", True, (30, 30, 150))
+        fenetre.blit(textemoyen, (moyenrect[0] + 10, moyenrect[1] + 10))
+        pygame.draw.rect(fenetre, (150, 0, 0), difficilerect,8)
+        textedifficile = daydream27.render("Difficile", True, (30, 30, 150))
+        fenetre.blit(textedifficile, (difficilerect[0] + 10, difficilerect[1] + 10))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                continuer = False
+        if pygame.mouse.get_pressed()[0] == False:
+                check6 = False
+        if pygame.mouse.get_pressed()[0] == True and check6 == False:
+                check6 = True
+                pos = pygame.mouse.get_pos()
+                if facilerect.collidepoint(pos):
+                    niveau = "facile"
+                    choisir = False
+                elif moyenrect.collidepoint(pos):
+                    niveau = "moyen"
+                    choisir = False
+                elif difficilerect.collidepoint(pos):
+                    niveau = "difficile"
+                    choisir = False
+                    
 def transition(roundn):
-    fenetre.blit(fond,(-1,-1))
+    fenetre.blit(fond,(0,0))
     surfaceroundt = daydream50.render("Round " + str(roundn), True, (30, 30, 150))
     fenetre.blit(surfaceroundt, (220,350))
     pygame.mixer.music.load("round.mp3")
@@ -169,7 +214,8 @@ def transition(roundn):
     pygame.time.wait(2000)
 
 def fin():
-    global score, ducksmort, duckspawn, check4
+    global score, ducksmort, duckspawn
+    pygame.mouse.set_visible(True)
     appreciation = ""
     if score >= 5000:
         appreciation = "INCROYABLE !"
@@ -179,31 +225,31 @@ def fin():
         appreciation = "Pas mal du tout !"
     else:
         appreciation = "Tu peux mieux faire !"
-    fenetre.fill((20, 20, 20))
-    pygame.draw.rect(fenetre, (30,30,150) , bord0, 8)
-    surfacefin = daydream50.render("FIN DE PARTIE", True, (30, 30, 150))
-    fenetre.blit(surfacefin, (120, 20))
-    surfacescore = daydream50.render("Score : "+str(score), True, (255, 255, 0))
-    fenetre.blit(surfacescore, (20, 220))
-    surfaceapp = daydream27.render(appreciation, True, (0, 200, 0))
-    fenetre.blit(surfaceapp, (20, 330))
-    pygame.draw.rect(fenetre, (30, 30, 150), rectquitter, 8)
-    surfacequit = daydream50.render("QUITTER", True, (255, 255, 255))
-    fenetre.blit(surfacequit, (rectquitter[0] + 10, rectquitter[1] + 10))
-    surfaceratio = daydream27.render(str(ducksmort)+ "/" + str(totducks) + " canards abattus", True, (255, 255, 255))
-    fenetre.blit(surfaceratio, (20, 290))
     pygame.mixer.music.load("win.mp3")
     pygame.mixer.music.play()
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            continuer = False
-    if pygame.mouse.get_pressed()[0] == False:
-        check4 = False
-    if pygame.mouse.get_pressed()[0] == True and check4 == False :
-        check4 = True
-        if rectquitter.collidepoint(event.pos):
-           continuer = False
+    final = True
+    while final == True:
+        fenetre.fill((20, 20, 20))
+        pygame.draw.rect(fenetre, (30,30,150) , bord0, 8)
+        surfacefin = daydream50.render("FIN DE PARTIE", True, (30, 30, 150))
+        fenetre.blit(surfacefin, (120, 30))
+        surfacescore = daydream50.render("Score : "+str(score), True, (255, 255, 0))
+        fenetre.blit(surfacescore, (20, 220))
+        surfaceapp = daydream27.render(appreciation, True, (0, 200, 0))
+        fenetre.blit(surfaceapp, (20, 330))
+        pygame.draw.rect(fenetre, (30, 30, 150), rectquitter, 8)
+        surfacequit = daydream50.render("QUITTER", True, (255, 255, 255))
+        fenetre.blit(surfacequit, (rectquitter[0] + 10, rectquitter[1] + 10))
+        surfaceratio = daydream27.render(str(ducksmort)+ "/" + str(totducks) + " canards abattus", True, (255, 255, 255))
+        fenetre.blit(surfaceratio, (20, 290))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                continuer = False
+            if pygame.mouse.get_pressed()[0] == True:
+                if rectquitter.collidepoint(event.pos):
+                    pygame.quit()
+                    sys.exit()
               
 def controles():
     global positionlunette, check, balles, score, rounds, check1, iconligne2, iconligne1, ducksmort
@@ -241,7 +287,9 @@ tic = 0
 intervalle = 10
 clock = pygame.time.Clock()
 debut()
+leveled()
 transition(0)
+tempsdebut = pygame.time.get_ticks()
 Continuer = True
 while Continuer == True:
    clock.tick(50)
@@ -256,6 +304,7 @@ while Continuer == True:
    if tic >= intervalle:
         tic = 0
         frame = (frame + 1) % 3
+        
    if rounds < len(duckrounds):
      while duckspawn < duckrounds[rounds]:
        ducks.append((random.randint(50, 600), random.randint(50, 423)))
@@ -264,9 +313,8 @@ while Continuer == True:
        ducketat.append("alive")
        verticaldirection.append(random.choice([0, 1])) 
        duckspawn = duckspawn +1
-   else:
-    fin()
-       
+
+    
    for i in range(len(ducks)-1, -1, -1):
     if ducketat[i] == "dead" and ducks[i][1] > 423 or tempsecoule >= 5 and ducks[i][1] < -100:
         del ducks[i]
@@ -294,11 +342,15 @@ while Continuer == True:
       if ducks[i][0] < 0 or ducks[i][0] > 700:
         directions[i] *= -1
         
-   if len(ducks) == 0 and ducksmort<=totducks:
-      pygame.time.wait(500)
-      rounds = rounds + 1
-      duckspawn = 0
-      transition(rounds)
-      tempsdebut = pygame.time.get_ticks()
+   if len(ducks) == 0 and ducksmort < totducks:
+        pygame.time.wait(500)
+        rounds += 1
+        if rounds < len(duckrounds):
+            duckspawn = 0
+            transition(rounds)
+            tempsdebut = pygame.time.get_ticks()
+        else:
+           while True:
+             fin()
      
-pygame.quit()      
+pygame.quit()   
